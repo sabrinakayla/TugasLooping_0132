c97d06e127599d286695649f245c632938c7b6b9
sabrinakayla <sabrinakuliah10@gmail.com>
2026-04-02 19:23:09 +0700

    finalisasi return fungsi bonacci

--- looping.cpp
+++ looping.cpp
@@ -21,3 +21,11 @@ bool isFibonacci(int n) {
     int a = 0, b = 1;
     if (n == a || n == b) return true;
     int c = a + b;
+    while (c <= n) {
+if (c == n) return true;
+a = b;
+b = c;
+c = a + b;
+}
+return false;
+}
